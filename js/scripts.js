d76096386403c41b2fe61dc45cf0db7af3ca6af5
ZephonMakale <mzephon@gmail.com>
2020-10-19 19:29:22 +0300

    modify the scrpts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,4 @@
-// $(document).ready(function() {
-//     function updateTotal(){
-//         var crust = 100;
-//         var toppings = 150;
-//         var tot = 0;
-//         tot += f.sizes.value * 3 +
-//                f.toppings.value * 3.5 +
-//                f.crust.value * 12 +
-//                f.delivery.value * 12;
-//         document.getElementById("cost").value = tot.toFixed(2);
-//       }
-
-//       var f = document.forms['myForm'];
-//       var sel = document.getElementsByTagName("select");
-
-//       for(var i=0; i<sel.length; i++) {
-//         sel[i].onchange = function(){updateTotal()};
-//       }
-
-//       f.onsubmit = function(){
-//         alert("Your total cost will be: \\$" + f.cost.value);
-//         return false;
-//       }
-// });
-
+mdoi
 function myFunction() {
   var size = parseInt(document.getElementById("sizes").value);
   
